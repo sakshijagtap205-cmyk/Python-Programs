d9081034136dde68cd26edfe819cbe4dbe99b339
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-06-14 18:50:07 +0530

    Add files via upload

---
+++ SequenceX.py
@@ -0,0 +1,16 @@
+Data1 = [10,20,30,40] #list
+Data2 = (10,20,30,40) #Tuple
+Data3 = {10,20,30,40} #Set
+Data4 = {"A": 10, "B": 20, "C": 30, "D":40}  #dict
+Data5 = "Maharashtra"    #str
+
+
+print(type(Data1))
+print(type(Data2))
+
+print(type(Data3))
+print(type(Data4))
+
+print(type(Data5))
+print(len(Data5))
+
