b7f0f9862a929ed82baef70d3fa758ad5dc0b627
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-06-21 16:19:05 +0530

    Add multiply function and user input for multiplication

---
+++ UserDefinedFunction.py
@@ -0,0 +1,8 @@
+def multiply(a , b):
+    return a *b
+
+num1 = int(input("enter first number"))
+num2 = int(input("Enter second number"))
+
+result = multiply(num1 , num2)
+print("Multiplication =", result)
