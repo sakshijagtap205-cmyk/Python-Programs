11d159ad1305c3055c01eac464758ed08c44d3ab
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-07-09 23:20:52 +0530

    Add files via upload

---
+++ AdditionVariable.py
@@ -0,0 +1,13 @@
+NO1 =0      #variable
+NO2 =0   #due to that 0 it become integer (same as float n etc)
+
+print("Enter first number:")
+NO1 = input()
+
+print("Enter Second number:")
+NO2 = input()
+
+
+Ans = NO1 + NO2
+
+print("Addition is:", Ans)
