4333fdadb8638a97e73f4911ffda9ef2096f3187
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-07-29 21:25:49 +0530

    Add files via upload

---
+++ Inheritance2.py
@@ -0,0 +1,12 @@
+
+
+
+class Base:
+    def __init__(self):
+        print("Inside Base Constructure")
+   
+class Derived(Base): #python
+    def __init__(self):
+        print("Inside Derived Constructure")
+
+bobj = Base()
