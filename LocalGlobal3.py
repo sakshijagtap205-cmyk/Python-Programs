6d30358c36423b25f957139a3b8594c671e7a27e
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-08-15 20:13:13 +0530

    Add files via upload

---
+++ LocalGlobal3.py
@@ -0,0 +1,9 @@
+no = 11                                           
+def Display():   
+  
+  no = 21
+  print("from Display:",no)
+  
+print("Before :", no)                                
+Display()                                        
+print("After:", no)                                         
