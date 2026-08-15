69cb621b36732f73a9267156667b5a4b2cb4dc4c
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-08-15 20:08:13 +0530

    Add files via upload

---
+++ LocalGlobal1.py
@@ -0,0 +1,11 @@
+no = 11                                   #Global Variable NO
+
+def Display():                          #function
+  print("From Display,:", no)
+   
+
+def Demo():                            #function
+    print("From Demo,:", no)
+
+Display()                              #function call
+Demo()                                 #function call
