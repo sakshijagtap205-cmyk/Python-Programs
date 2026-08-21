5a4a2bc55522313be0f9bf8fd30272a60cfb6901
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-08-21 22:51:12 +0530

    Add files via upload

---
+++ MultiProcessing1.py
@@ -0,0 +1,39 @@
+import time
+import multiprocessing
+
+def SumEven(no):
+    sum =0
+    for i in range(2 , no , 2):
+        sum = sum +i
+        
+    print("Summation of even :", sum)
+
+def SumOdd(no):
+    
+    sum =0
+    
+    for i in range(1,no,2):
+      sum = sum +i
+        
+    print("Summation of odd :", sum)
+        
+def main():
+    
+    start_time = time.perf_counter()
+    
+    t1 = multiprocessing.Process(target =SumEven , args=(10000000,))
+    t2 = multiprocessing.Process(target =SumOdd , args=(10000000,))
+    
+    t1.start()
+    t2.start()
+    
+    t1.join
+    t2.join
+    
+    end_time = time.perf_counter()
+    
+    print(f"time required is :{end_time - start_time:.4}")
+  
+  
+if __name__ == "__main__":
+    main()
