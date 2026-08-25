f798b25fcb258817d7df152258400b52ddf5dbb2
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-08-25 19:26:21 +0530

    Add files via upload

---
+++ MultiThreading7.py
@@ -0,0 +1,37 @@
+import time
+import threading
+
+def SumEven(no):
+    sum =0
+    
+    for i in range(2 , no , 2):
+        sum = sum +i
+        
+    print("Summation of even :", sum)
+
+def SumOdd(no):
+    
+    sum =0
+    
+    for i in range(1,no,2):
+      sum = sum +i
+        
+    print("Summation of odd :", sum)
+        
+def main():
+    
+    start_time = time.perf_counter()
+    
+    t1 = threading.Thread(target =SumEven , args=(1000000,))
+    t2 = threading.Thread(target =SumOdd , args=(1000000,))
+    
+    t1.start()
+    t2.start()
+    
+    end_time = time.perf_counter()
+    
+    print(f"time required is :{end_time - start_time:.4}")
+  
+  
+if __name__ == "__main__":
+    main()
